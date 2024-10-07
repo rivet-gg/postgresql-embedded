d27a0b0e37a96c4dd639c9b71e52a873d8b0844a
Nathan Flurry <developer@nathanflurry.com>
2024-10-07 14:04:19 -0700

    fix: handle x86_64-pc-windows-gnu target correctly

--- postgresql_archive/src/target.rs
+++ postgresql_archive/src/target.rs
@@ -1,4 +1,11 @@
 pub fn get_target_triple() -> String {
-    std::env::var("OVERRIDE_TARGET").unwrap_or_else(|_| target_triple::TARGET.to_string())
+    let target = std::env::var("OVERRIDE_TARGET").unwrap_or_else(|_| target_triple::TARGET.to_string());
+
+    // Replace gnu targets with msvc
+    if target.starts_with("x86_64-pc-windows-") {
+        "x86_64-pc-windows-msvc".to_string()
+    } else {
+        target
+    }
 }
 
